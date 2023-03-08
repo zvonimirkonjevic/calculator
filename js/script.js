324a8dd207663bda9e7c92b2a09c3b5981923086
Zvonimir <zvonimir.konjevic@gmail.com>
2023-03-08 12:28:36 +0100

    Create function operate()
    Create function operate() that for passed operation symbol and two numbers calls
    desired operation and returns result of that operation.

--- js/script.js
+++ js/script.js
@@ -13,3 +13,10 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+function operate(leftNumber, rightNumber, operator){
+    if(operator === '+'){ return add(leftNumber, rightNumber);}
+    if(operator === '-'){ return subtract(leftNumber, rightNumber);}
+    if(operator === '÷'){ return divide(leftNumber, rightNumber);}
+    if(operator === 'x'){ return multiply(leftNumber, rightNumber);}
+}
