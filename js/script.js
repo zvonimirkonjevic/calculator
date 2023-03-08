5ff6166b9a58e2b74dc8414c0e6f910e0ce54c54
Zvonimir <zvonimir.konjevic@gmail.com>
2023-03-08 12:23:28 +0100

    Create function multiply()
    Created function multiply() that calculates product of two numbers passed to
    the function.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ function add(a, b){
     return a + b;
 }
 
-function subtract(leftNumber, rightNumber){
-    return leftNumber - rightNumber;
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
 }
